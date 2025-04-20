0cd344c6622142ac6930a2cc091bad6bff65ef32
Bryce Carter <carterbryce54@gmail.com>
2025-04-19 19:46:29 -0600

    Home button

--- frontend/src/pages/EntertainersPage.tsx
+++ frontend/src/pages/EntertainersPage.tsx
@@ -1,9 +1,12 @@
+import { useNavigate } from 'react-router-dom';
 import EntertainerList from '../components/EntertainerList';
 
 function EntertainersPage() {
+    const navigate = useNavigate();
   return (
     <>
       <div className="container mt-4">
+        <button onClick={() => navigate("/")}>Back to Home</button>
         <div>
           <EntertainerList />
         </div>
